a288485b1727cde22ce7d3c92c91c8936c3c9575
doorgan <dorgandash@gmail.com>
2021-03-17 22:17:57 +0000

    Move docs

--- .eleventy.js
+++ .eleventy.js
@@ -37,7 +37,7 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: "docs",
+      input: "doc",
     },
   };
 }
--- snowpack.config.js
+++ snowpack.config.js
@@ -34,6 +34,6 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    out: 'build',
+    out: 'docs',
   },
 };
